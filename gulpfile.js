e896539123121a1deb78c362137668e49c42193d
at15 <at15@outlook.com>
2015-12-16 12:53:04 +0800

    Add travis.yml

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,22 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 
-// TODO: use tsconfig.json to build src and test
 var srcProject = ts.createProject('src/tsconfig.json');
 var testProject = ts.createProject('test/tsconfig.json');
 
 gulp.task('build-src', function () {
-    var tsResult = srcProject.src() // instead of gulp.src(...)
+    var tsResult = srcProject.src()
         .pipe(ts(srcProject));
 
     return tsResult.js.pipe(gulp.dest('src'));
 });
 
-gulp.task('build-test', function () {
-    var tsResult = testProject.src() // instead of gulp.src(...)
+gulp.task('build-test', ['build-src'], function () {
+    var tsResult = testProject.src()
         .pipe(ts(testProject));
 
     return tsResult.js.pipe(gulp.dest('test'));
+});
+
+gulp.task('build', ['build-test'], function () {
 });
