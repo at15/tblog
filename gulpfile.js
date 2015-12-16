f063f340500103b36d3a45216facf8f1834ce853
at15 <at15@outlook.com>
2015-12-16 12:16:44 +0800

    gulp build-src is working

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 /**
  * Created by gpl on 15/12/16.
  */
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+
+// TODO: use tsconfig.json to build src and test
+var srcProject = ts.createProject('src/tsconfig.json');
+var testProject = ts.createProject('test/tsconfig.json');
+
+gulp.task('build-src', function () {
+    var tsResult = srcProject.src() // instead of gulp.src(...)
+        .pipe(ts(srcProject));
+
+    return tsResult.js.pipe(gulp.dest('.'));
+});
